632badaf16a5abff5fd2a19a727afb7ac6d320e4
dabgrows-oss <dabgrows@gmail.com>
2026-01-01 22:00:24 -0800

    Update Index.js

--- pages/Index.js
+++ pages/Index.js
@@ -1,1 +1,8 @@
-dabs-dynasty-selections
+export default function Home() {
+  return (
+    <div>
+      <h1>Dabs Dynasty Selections</h1>
+      <p>Welcome to my first website.</p>
+    </div>
+  );
+}
