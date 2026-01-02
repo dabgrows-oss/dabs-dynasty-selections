c67ce62d3cd58bc66173a722791db2ff1d51d43f
dabgrows-oss <dabgrows@gmail.com>
2026-01-01 22:22:05 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Dabs Dynasty Selections</h1>
-      <p>Welcome to my first website.</p>
-    </div>
+    <main style={{ padding: 20 }}>
+      <h1>Welcome to Dabs Dynasty Selections</h1>
+      <p>Your site is live.</p>
+    </main>
   );
 }
